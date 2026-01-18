19074ccf8cb03940e779c098c4acdae9249e9b00
Profray <professorray169@gmail.com>
2026-01-18 06:19:57 -0800

    Refactor Deploy script for NFTMarketplace deployment
    Updated the deployment script to use environment variable for NFT address and changed contract imports.

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -1,22 +1,19 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.30;
+pragma solidity ^0.8.20;
 
 import "forge-std/Script.sol";
-import "../src/SimpleNFT.sol";
-import "../src/NFTMarketplace.sol";
+import "../src/nftmarket.sol";
+import "../src/myNFT.sol";
+
 
 contract DeployMarketplace is Script {
     function run() external {
-        // Start recording transactions to send to the blockchain
-        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-        vm.startBroadcast(deployerPrivateKey);
+        address nftAddress = vm.envAddress("NFT_ADDRESS");
 
-        // 1. Deploy the NFT contract
-        SimpleNFT nft = new SimpleNFT();
-
-        // 2. Deploy the Marketplace with the NFT's address
-        NFTMarketplace marketplace = new NFTMarketplace(address(nft));
-
+        vm.startBroadcast();
+        NFTMarketplace marketplace = new NFTMarketplace(nftAddress);
         vm.stopBroadcast();
+
+        console.log("Marketplace deployed at:", address(marketplace));
     }
 }
